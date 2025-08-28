c68fac91cb307eb55a8d127feb953384ab102752
Samir D. Araah <samiraraah18@gmail.com>
2025-08-28 14:22:00 +0800

    models and admin working properly

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3u8)ra*-cepe359r_7q@%^$vwyk$kvkf+o%==-=wy4hpb)o%jd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
