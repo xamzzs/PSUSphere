faf998d725fba336ef09b409ea95fb083746fc73
Ruzzyyyy <ruzzyy.arres@gmail.com>
2025-09-24 23:09:37 +0800

    modify settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-3u8)ra*-cepe359r_7q@%^$vwyk$kvkf+o%==-=wy4hpb)o%jd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['xammzs.pythonanywhere.com', '127.0.0.1',]
+ALLOWED_HOSTS = ['xammzs.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -48,7 +49,11 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.github',
 ]
 
-SITE_ID = 3
+if "pythonanywhere" in socket.gethostname():
+    SITE_ID = 2 # production site (psusphere.pythonanywhere.com)
+else:
+    SITE_ID = 1 # local site (127.0.0.1:8000)
+
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
